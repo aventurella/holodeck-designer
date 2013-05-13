869b8f1e22effd5010bb28d5f730e19e4c489661
Dino Petrone <dino@madeinhaus.com>
2013-05-13 10:30:43 -0700

    tweeked crap and added draggable

--- build.js
+++ build.js
@@ -2,6 +2,7 @@
     paths: {
         requireLib: 'src/resources/js/vendor/require',
         jquery: 'src/resources/js/vendor/jquery',
+        jqueryui: 'src/resources/js/vendor/jquery-ui',
         underscore: 'src/resources/js/vendor/underscore',
         bootstrap: 'src/resources/js/vendor/bootstrap',
         mustache : 'src/resources/js/vendor/mustache',
--- src/resources/js/designer.js
+++ src/resources/js/designer.js
@@ -2,7 +2,8 @@ require([
     'jquery',
     'underscore',
     'bootstrap',
-    'requireLib'
+    'requireLib',
+    'jqueryui',
 ], function($, _, Mustache){
 
     var currentPanel = false;
@@ -11,100 +12,31 @@ require([
     var transitionOut = 'panel-move-to-left';
     var isAnimating = false;
     var animationEndEvent = 'webkitAnimationEnd';
+    var $leftMenu = $('.cbp-vimenu')
+    var $submenus = $leftMenu.find('a[data-menuclass]')
+    var $controls = $('.controls')
+    
 
     function initialize(){
         initializeMenu();
+        initializeDragMenu();
         panelContext = $('.column.controls');
-        currentPanel = panelForClass('hue-lights');
+    }
+
+
+    function initializeDragMenu(){
+        $( "#sortable1, #sortable2" ).sortable({
+          connectWith: ".connectedSortable"
+        }).disableSelection();
     }
 
     function initializeMenu(){
-        var lightItem = $('.menu .cbp-vimenu .menu-light');
-        var controlItem = $('.menu .cbp-vimenu .menu-gear');
-
-        lightItem.on('click', onLightsClicked);
-        controlItem.on('click', onControlClicked);
+        $submenus.on('click',_.bind(onLeftMenuItemClick,this))
     }
 
-    function onLightsClicked(e){
-        e.preventDefault();
-        nextPanel = panelForClass('hue-lights');
-
-        if(shouldTransitionPanel(nextPanel)){
-            transitionPanelFromTo(currentPanel, nextPanel);
-        }
-    }
-
-    function onControlClicked(e){
-        e.preventDefault();
-        nextPanel = panelForClass('hue-control');
-
-        if(shouldTransitionPanel(nextPanel)){
-            transitionPanelFromTo(currentPanel, nextPanel);
-        }
-    }
-
-    function shouldTransitionPanel(nextPanel){
-
-        if( isAnimating ) {
-            return false;
-        }
-
-        var nextParts = nextPanel.attr('class').split(' ');
-        var currentParts = currentPanel.attr('class').split(' ');
-
-        var nextId = _.filter(
-            nextParts,
-            function(each){ return each.match(/^hue-/); });
-
-        var currentId = _.filter(
-            currentParts,
-            function(each){ return each.match(/^hue-/); });
-
-        if (nextId[0] != currentId[0]){
-            isAnimating = true;
-            return true;
-        }
-
-        return false;
-    }
-
-    function transitionPanelFromTo(outPanel, inPanel){
-
-        inPanel.addClass('current');
-
-        outPanel.addClass(transitionOut).on(
-            animationEndEvent,
-            function(){
-                outPanel.off(animationEndEvent);
-            }
-        );
-
-        inPanel.addClass(transitionIn).on(
-            animationEndEvent,
-            function(){
-                inPanel.off(animationEndEvent);
-                onAnimationEnd(outPanel, inPanel);
-            }
-        );
-    }
-
-    function onAnimationEnd(outPanel, inPanel){
-        currentPanel = inPanel;
-        isAnimating = false;
-        resetPanel(outPanel, inPanel);
-    }
-
-    function resetPanel( outPanel, inPanel ) {
-        outPanel.removeClass('current');
-        outPanel.removeClass(transitionOut);
-
-        inPanel.removeClass(transitionIn);
-    }
-
-
-    function panelForClass(className){
-        return panelContext.find('.' + className);
+    function onLeftMenuItemClick(event){
+        var menuClass = $(event.target).data('menuclass')
+        $controls.find('.current').removeClass('current')
     }
 
     initialize();
