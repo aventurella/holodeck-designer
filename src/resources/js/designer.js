53f46e4d1b7eece7b2702c7a08ff3333434c713c
Adam Venturella <aventurella@gmail.com>
2013-05-19 10:23:55 -0700

    live preview

--- src/resources/js/designer.js
+++ src/resources/js/designer.js
@@ -19,40 +19,4 @@ require([
     function loadHuesComplete(data){
         mainMenu.setHues(data);
     }
-    // var currentPanel = false;
-    // var panelContext = false;
-    // var transitionIn = 'panel-move-from-right';
-    // var transitionOut = 'panel-move-to-left';
-    // var isAnimating = false;
-    // var animationEndEvent = 'webkitAnimationEnd';
-    // var $leftMenu = $('.cbp-vimenu')
-    // var $submenus = $leftMenu.find('a[data-menuclass]')
-    // var $controls = $('.controls')
-
-
-    // function initialize(){
-    //     initializeMenu();
-    //     initializeDragMenu();
-    //     panelContext = $('.column.controls');
-    // }
-
-
-    // function initializeDragMenu(){
-    //     $( "#sortable1, #sortable2" ).sortable({
-    //       connectWith: ".connectedSortable"
-    //     }).disableSelection();
-    // }
-
-    // function initializeMenu(){
-    //     $submenus.on('click',_.bind(onLeftMenuItemClick,this))
-    // }
-
-    // function onLeftMenuItemClick(event){
-    //     var menuClass = $(event.target).data('menuclass')
-    //     $controls.find('.current').removeClass('current')
-    //     $('.'+menuClass).addClass('current').addClass('current')
-    // }
-
-    // initialize();
-
 });
